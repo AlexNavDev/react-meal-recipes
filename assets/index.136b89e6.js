38a73fa51937787f1928c6ae41dcc7a88d42ce33
Alejandro <navarro.alejandro@outlook.es>
2022-11-01 22:32:44 -0600

    Updates

--- assets/index.136b89e6.js
+++ assets/index.136b89e6.js
@@ -562,6 +562,7 @@ align-items: center;
   display: ${e=>e.isOpen};
 `,A1=We.article`
   width: 100%;
+  height: 650px;
   background-image: url(${Op});
   background-size: 100% 100%;
   background-repeat: no-repeat;
